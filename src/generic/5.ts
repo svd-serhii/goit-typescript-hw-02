d18aa143b32ec7bed8171081f4666297a5842fba
Serhii Demchenko <svdemchenko@gmail.com>
2024-05-18 18:57:57 +0300

    2 5-8 tasks

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,14 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<TKey, TValue> {
+	key: TKey;
+	value: TValue;
 }
 
+const pair: KeyValuePair<string, number> = {
+	key: "id",
+	value: 2,
+};
+console.log(pair);
 export {};
